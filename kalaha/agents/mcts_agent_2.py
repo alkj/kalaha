151a4af5a15ef0078734968dd9f2f33a78e35815
alexander <alek1kjeldsen@gmail.com>
2023-03-20 15:06:01 +0100

    mcts structure complete

--- kalaha/agents/mcts_agent_2.py
+++ kalaha/agents/mcts_agent_2.py
@@ -1,4 +1,5 @@
 import copy
+import math
 import random
 
 from kalaha.agents.agent import Agent
@@ -9,8 +10,7 @@ class Node:
     def __init__(self, prior, game):
         self.prior = prior
         self.game = game
-
-        self.children = []
+        self.children = {}
         self.visit_count = 0
         self.value_sum = 0
         self.state = None
@@ -18,123 +18,157 @@ class Node:
     def value(self):
         return self.value_sum / self.visit_count
 
+    def select_child(self):
+        best_score = -48
+        best_action = -1
+        best_child = None
+
+        for action, child in self.children.items():
+            score = self.ubc_score(child)  # ucb_score(self, child)
+            if score > best_score:
+                best_score = score
+                best_action = action
+                best_child = child
+
+        return best_action, best_child
+
+    def expand(self, game, move_to_play, ):
+        for c in self.game.get_possible_moves():
+            game_copy = copy.deepcopy(c)
+            game_copy.move_marbles(c)
+            self.children[c] = Node(self.value_sum, make_move(game_copy, c))
+
+    def is_expanded(self):
+        return self.children != {}
+
+    def ubc_score(self, child):
+        prior_score = child.prior * math.sqrt(self.visit_count) / (child.visit_count + 1)
+        return child.value() + prior_score
+
+    def highest_value_choice(self):
+        best_index = -1
+        highest_score = -48
+        for i, c in self.children:
+            if c.value() > highest_score:
+                highest_score = c.value()
+                best_index = i
+        return best_index
+
+
+def simulate_times(game, param) -> float:
+    simulations = list(map(lambda n: simulate(game), [0] * param))
+    return sum(simulations)
+
+
+def simulate(input_game):
+    if input_game.winner == Player.TOP:
+        return 1.0
+    if input_game.winner == Player.BOTTOM:
+        return 0.0
+    if input_game.winner is None:  # it's a draw
+        return 0.5
+    if len(input_game.get_possible_moves()) == 0:
+        print("empty")
+    random_choice = random.choice(input_game.get_possible_moves())
+    game_copy = copy.deepcopy(input_game)
+    game_copy.move_marbles(random_choice)
+    return simulate(game_copy)
+
+
+def make_move(game, move):
+    deepcopy = copy.deepcopy(game)
+    deepcopy.move_marbles(move)
+    return deepcopy
+
 
 class MCTSAgent2(Agent):
 
     def __init__(self):
+        self.root = None
         self.max_depth = 2
 
     """
-    An human agent providing a CLI to choose the moves
+    SELECTION
+    EXPANSION
+    SIMULATION
+    UPDATE
+    
+    
     """
 
     def choose(self, input_game) -> int:
-        choice = self.mcts(copy.deepcopy(input_game), 0)
+        choice = self.mcts(input_game).highest_value_choice()
         print(self, "chooses:", choice, "for", input_game.current_player)
         return choice
 
-    def mcts(self, input_game, depth: int) -> int:
+    def mcts(self, input_game, sampling=10, simulations=10) -> Node:
 
-        best_move = -1
+        self.root = Node(0, copy.deepcopy(input_game))
+        moves = self.root.game.get_possible_moves()  # list of moves
 
-        game = copy.deepcopy(input_game)
+        # Expansion
+        for m in moves:
+            self.root.children[m] = Node(0, make_move(self.root.game, m))
 
-        # the top player wants to maximize the score of the board
-        if game.current_player == Player.TOP:
-            maximum_score = -48
-            for i in game.get_possible_moves():
-                # print("checking move ", i)
-                game_copy = copy.deepcopy(game)
-                game_copy.move_marbles(i)
-                # if the player after the move is the top player again, then the score should be maximized
-                if game_copy.current_player == Player.TOP:
-                    score = self.maximize(game_copy, depth + 1)
-                    if score >= maximum_score:
-                        maximum_score = score
-                        best_move = i
+        # Simulation
+        for child in self.root.children:
+            self.root.children[child].value_sum += simulate_times(self.root.children[child].game, sampling)
+            self.root.children[child].visit_count += sampling
 
-                else:
-                    score = self.minimize(game_copy, depth + 1)
-                    if score >= maximum_score:
-                        maximum_score = score
-                        best_move = i
-            #  print("maximum score:", maximum_score)
+        # Update
+        for child in self.root.children:
+            self.root.value_sum += self.root.children[child].value_sum
+            self.root.visit_count += self.root.children[child].visit_count
 
-        else:  # bottom player
-            minimum_score = 48
-            for i in game.get_possible_moves():
-                # print("checking move ", i)
-                game_copy = copy.deepcopy(game)
-                game_copy.move_marbles(i)
-                if game_copy.current_player == Player.TOP:
-                    score = self.maximize(game_copy, depth + 1)
-                    if score <= minimum_score:
-                        minimum_score = score
-                        best_move = i
-                else:
-                    score = self.minimize(game_copy, depth + 1)
-                    if score <= minimum_score:
-                        minimum_score = score
-                        best_move = i
-            #  print("minimum score:", minimum_score)
-        return best_move
+        print(self.root.value())
+        for child in self.root.children:
+            print(self.root.children[child].value(), end=" : ")
 
-    def minimize(self, input_game, depth: int) -> float:
-        if input_game.winner == Player.TOP:
-            return 1
-        if input_game.winner == Player.BOTTOM:
-            return 0
-        if input_game.winner is None:
-            return 0.5
-        if depth == self.max_depth:  # run simulations
-            simulations = []
-            for i in range(10):
-                simulations.append(self.simulate(input_game))
-            return sum(simulations) / len(simulations)  # average win rate for top player
+        path = [self.root]
 
-        minimum_score = 48
+        for i in range(simulations):  # number of simulations
+            node = self.root
+            while node.is_expanded():
+                move, child = self.root.select_child()
+                path.append(child)
+                node = child
 
-        game = copy.deepcopy(input_game)
-        for i in game.get_possible_moves():
-            game_copy = copy.deepcopy(game)
-            game_copy.move_marbles(i)
-            if game_copy.current_player == Player.TOP:
-                score = self.maximize(game_copy, depth + 1)
+            parent = path[-2]
+
+            # is game finished ?
+            # who is playing ?
+            # can it be expanded ?
+            if node.game.winner == Player.BLANK:
+                moves = node.game.get_possible_moves()  # list of moves
+
+                # Expansion
+                for m in moves:
+                    node.children[m] = Node(0, make_move(node.game, m))
+
+                # Simulation
+                for child in node.children:
+                    node.children[child].value_sum += simulate_times(node.children[child].game, sampling)
+                    node.children[child].visit_count += sampling
+
+                # Update
+                for child in node.children:
+                    node.value_sum += node.children[child].value_sum
+                    node.visit_count += node.children[child].visit_count
+
+                self.backpropagate(path, node.value())
             else:
-                score = self.minimize(game_copy, depth + 1)
-            if score <= minimum_score:
-                minimum_score = score
-        return minimum_score
+                # the game is over. either a win or a draw.
+                win = 0
+                if node.game.winner == Player.TOP:
+                    win = 1
+                self.backpropagate(path, win)
 
-    def maximize(self, input_game, depth: int) -> float:
-        if input_game.winner == Player.TOP:
-            return 1
-        if input_game.winner == Player.BOTTOM:
-            return 0
-        if input_game.winner is None:
-            return 0.5
-        if depth == self.max_depth:
-            simulations = []
-            for i in range(10):
-                simulations.append(self.simulate(input_game))
-            return sum(simulations) / len(simulations)
+        return self.root
 
-        maximum_score = -48
-
-        game = copy.deepcopy(input_game)
-        simulations = []
-        for i in game.get_possible_moves():
-            game_copy = copy.deepcopy(game)
-            game_copy.move_marbles(i)
-            if game_copy.current_player == Player.TOP:
-                score = self.maximize(game_copy, depth + 1)
-            else:
-                score = self.minimize(game_copy, depth + 1)
-            simulations.append(score)
-        return sum(simulations) / len(simulations)
-
-    def __str__(self):
-        return "MCTS Agent 2"
+    def backpropagate(self, path, win):
+        for node in reversed(path):
+            node.value_sum += win
+            node.visit_count += 1
 
     def simulate(self, input_game) -> int:
         if input_game.winner == Player.TOP:
@@ -145,3 +179,6 @@ class MCTSAgent2(Agent):
         game_copy = copy.deepcopy(input_game)
         game_copy.move_marbles(random_choice)
         return self.simulate(game_copy)
+
+    def __str__(self):
+        return "MCTS Agent 2"
